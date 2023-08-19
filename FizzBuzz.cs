806546e28c4162cd94e56e60f618ef909806a925
ShakhriyorToylokov1999 <118186892+ShakhriyorToylokov1999@users.noreply.github.com>
2023-08-19 12:39:25 +0500

    Add project files.

---
+++ FizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestingTask1
+{
+   public class FizzBuzz
+    {
+
+        private static string FIZZ = "FIZZ";
+        private static string BUZZ = "BUZZ";
+        
+        public void Run()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(checkFizzBuzz(i));
+            }
+        }
+
+        public static string checkFizzBuzz(int num)
+        {
+            if (num < 1 || num > 100)
+                throw new ArgumentException("The number should be between 1-100 ");
+
+            if ((num % 3 == 0) && (num % 5 == 0))
+                return FIZZ + BUZZ;
+
+            if (num % 3 == 0)
+                return FIZZ;
+
+            if (num % 5 == 0)
+                return BUZZ;
+
+            return num.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace UnitTestingTask1
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var fizzBuzz = new FizzBuzz();
+            fizzBuzz.Run();
+        }
+
+    }
+}
